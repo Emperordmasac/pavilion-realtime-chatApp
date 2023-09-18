7757a495dc3e3223b1bfa21944839956d408b29c
Abiola Olamilekan <abiolawalter2015@gmail.com>
2023-09-18 11:51:47 +0100

    feat: adding a friend

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,9 +1,41 @@
+import { authOptions } from '@/lib/auth'
 import { addFriendValidator } from '@/lib/validations/add-friend'
+import { getServerSession } from 'next-auth'
 
 export async function POST(req: Request) {
   try {
     const body = await req.json()
 
     const { email: emailToAdd } = addFriendValidator.parse(body.email)
+
+    const RestResponse = await fetch(
+      `${process.env.UPSTASH_REDIS_REST_URL}/get/user:email${emailToAdd}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`,
+        },
+        cache: 'no-store',
+      },
+    )
+
+    const data = (await RestResponse.json()) as { result: string | null }
+
+    const idToAdd = data.result
+
+    if (!idToAdd) {
+      return new Response('This user does not exist', { status: 400 })
+    }
+
+    const session = getServerSession(authOptions)
+    if (!session) {
+      return new Response('Unauthorized Access', { status: 401 })
+    }
+
+    if (idToAdd === session?.user?.id) {
+      return new Response(
+        'This action is invalid, you cannot add yourself as a friend',
+        { status: 400 },
+      )
+    }
   } catch (error) {}
 }
